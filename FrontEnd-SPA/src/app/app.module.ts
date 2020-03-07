b0a25172e6fe38bfa0c9744bf9332eeca2b5e21a
SyedShahbaz <syedshahbaz@live.co.uk>
2020-03-08 04:54:40 +0500

    Boots watch and ngx-Bootstrap

--- FrontEnd-SPA/src/app/app.module.ts
+++ FrontEnd-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -17,12 +19,14 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       ValueComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+    //   BrowserAnimationsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
