ebc72a9a60550a36d3a2f54d0cc6ba5614ebcd8e
Syed Haider <syed.haider@L-P-A.com>
2020-04-13 19:12:16 +0200

    Initial UI for client side

--- BackEnd.API/Helpers/AutoMapperProfiles.cs
+++ BackEnd.API/Helpers/AutoMapperProfiles.cs
@@ -15,6 +15,7 @@ namespace BackEnd.API.Helpers
             CreateMap<User, UserForDetailDto>()
                 .ForMember(dest => dest.PhotoURL,
                     opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
 
             CreateMap<User, UserForListDto>().ForMember(dest => dest.PhotoURL,
